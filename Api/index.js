763edbe553416d7447cbf18e4de45c48ca75c310
manuord <villalobosmanuelmatias@gmail.com>
2024-02-08 01:47:52 -0300

    database structure

--- Api/index.js
+++ Api/index.js
@@ -1,1 +1,14 @@
-const path = require('path')
+require('dotenv').config();
+
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+
+const port = process.env.PORT || 3001;
+
+conn.sync({force: false}).then(()=>{
+    // custiones de configuracion de la db / pendiente
+
+    server.listen(port, ()=>{
+        console.log(`Server running on port: ${port}`);
+    });
+});
