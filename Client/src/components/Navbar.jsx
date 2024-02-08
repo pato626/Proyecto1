e3188597e1ef0beedd241c798e04e51e33f0f2e0
pato <ruromar666@gmail.com>
2024-02-07 21:12:58 -0300

    primer commit

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Cart from "../assets/Cart.svg"
+const Navbar = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+
+                        <a href=""> <li>Inicio</li> </a>
+                        <a href=""> <li>Sobre nosotros</li></a>
+                        <a href=""> <li>Contacto</li></a>
+                        <a href=""> <li><img src="" alt="" /></li></a>
+
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
